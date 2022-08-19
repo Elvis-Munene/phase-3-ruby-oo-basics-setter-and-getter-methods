59909df8c316963e47fedccb15311c568292ff4a
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-08-19 15:52:32 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    def name=(value)
+        @name = value
+    end
 
+
+    def name
+        @name
+    end
+
+    def breed=(value)
+        @breed = value
+    end
+
+    def breed
+        @breed
+    end
 end
+dog1 = Dog.new
+dog1.name = "snoopy"
+dog1.breed = "German"
